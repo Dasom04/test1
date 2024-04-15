758088742a4d8ec2284d9e599913c67c9eaf40de
dasom <slz_952@naver.com>
2024-04-15 18:29:23 +0900

    카카오 인가, 토큰

---
+++ backup/appKakaoLog.java
@@ -0,0 +1,7 @@
+// 카카오 로그인시 rest키
+// link
+
+
+// # sns login property
+sns.kakao.app-key=	7bcde99152a4c14e5a7a9ca8cf16f110
+sns.kakao.redirect-uri=http://localhost:8181/auth/kakao
---
+++ backup/kakaojsp.java
@@ -0,0 +1,14 @@
+</style>
+</head>
+<body>
+<%@ include file="include/header.jsp"%>
+<div id="section"><h1 style="position: relative;top: 150px;">로그인</h1>
+    <button type="button" style="position: relative;top: 150px;border: none;background-color: white; cursor: pointer;" onclick="location.href='/kakao/login'">
+        <img src="../assets/img/kakaotest.png" height="50" width="50" >
+    </button>
+    <a href="/naver/login" style="position: relative;top: 150px;border: none;background-color: white; cursor: pointer;">
+        <img src="../assets/img/navertest.png"height="50" width="50" >
+    </a>
+    <button type="button" style="position: relative;top: 150px;border: none;background-color: white; cursor: pointer;">
+        <img src="../assets/img/googletest.png"height="50" width="50" >
+    </button>
